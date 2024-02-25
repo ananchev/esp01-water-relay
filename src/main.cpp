06c52f00f4441df2ae8be196f3c31d553aa29649
Anton Nanchev <ananchev@gmail.com>
2024-02-25 22:57:16 +0100

    manage 2 way relay module via UART

--- src/main.cpp
+++ src/main.cpp
@@ -4,82 +4,105 @@
 #include <ESPAsyncWebServer.h>
 #include <helperfunctions.h>
 
-
-
-// Wi-Fi and webserver settings 
-const char* ssid = "CasaDiMi";
-const char* password = "Muska1ova";
+// Wi-Fi and webserver settings
+const char *ssid = "ssid";
+const char *password = "password";
 AsyncWebServer server(80);
 
-
-
-// 
+// time variables
 long prevReadingTime = 0;
 long wateringInterval = 0;
+long postpone_seconds = 0;
 int remainingWateringTime;
 
+// Open the first relay： A0 01 01 A2
+// Close the first Relay： A0 01 00 A1
+// Open the second relay： A0 02 01 A3
+// Close the second Relay：A0 02 00 A2
 
-// Set relay GPIO
-const int sprinklerRelayPin = 0; // relay connected to  GPIO0
-const int dripIrrigationRelayPin = 1; // relay connected to  GPIO1
+// the 2-way relay is managed via serial interface
+const byte deactivate_relay_one[] = {0xA0, 0x01, 0x00, 0xA1};
+const byte activate_relay_one[] = {0xA0, 0x01, 0x01, 0xA2};
+const byte deactivate_relay_two[] = {0xA0, 0x02, 0x00, 0xA2};
+const byte activate_relay_two[] = {0xA0, 0x02, 0x01, 0xA3};
+const byte eol[] = {0x0d, 0x0a};
 
 // Store the relay states
+int sprinklerRelay = 0;
+int dripIrrigationRelay = 0;
 String sprinklerRelayState;
 String dripIrrigationRelayState;
 
-// Stores the interval for which watering was set 
+// Stores the interval for which watering was set
 String interval = "";
 
-
 // Replaces placeholders with the relay states (called on calling the 'index' page)
-String processor(const String& var){
+String processor(const String &var)
+{
   // Serial.println(var);
-  if(var == "SPRINKLER_STATE"){
-    if(digitalRead(sprinklerRelayPin)){
+  if (var == "SPRINKLER_STATE")
+  {
+    if (sprinklerRelay == 1)
+    {
       sprinklerRelayState = "ON";
     }
-    else{
+    else
+    {
       sprinklerRelayState = "OFF";
     }
     // Serial.print(relayState);
     return sprinklerRelayState;
   }
-  else if (var == "DRIP_IRRIGATION_STATE"){
-    if(digitalRead(dripIrrigationRelayPin)){
+  else if (var == "DRIP_IRRIGATION_STATE")
+  {
+    if (dripIrrigationRelay == 1)
+    {
       dripIrrigationRelayState = "ON";
     }
-    else{
+    else
+    {
       dripIrrigationRelayState = "OFF";
     }
     // Serial.print(relayState);
     return dripIrrigationRelayState;
   }
-  else {
+  else
+  {
     return "Nothing";
   }
 };
 
+// this is dummy callback function to test the workaround for delay with AsyncWebServer
+void aR1(AsyncWebServerRequest *request)
+{
+  Serial.write(activate_relay_two, sizeof(activate_relay_two));
 
-
-void setup() {
-
-  // Serial.begin(115200);
-
-  // Initialize SPIFFS 
-  if(!SPIFFS.begin()){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-  return;
+  auto seconds = 1;
+  auto end = millis() + seconds * 1000;
+  // this loop, when empty, causes a reboot after about 3 seconds
+  // but imagine this is not an empty loop, but a real stuff that is just taking a bit of time…
+  while (end > millis())
+  {
+    // yield(); // panic, crash, restart
+    // ESP.wdtFeed(); // no crashes even for long times, but everything else is being blocked
   }
 
-  // Swap the GPIO 1 (TX) pin to a GPIO.
-  // To use TX RX as GPIO Serial.begin() must be removed from code.
-  pinMode(1, FUNCTION_3);
+  Serial.write(activate_relay_one, sizeof(activate_relay_one));
 
-  // Configure the relay pins as output and set to Low
-  pinMode(sprinklerRelayPin,OUTPUT);
-  digitalWrite(sprinklerRelayPin, LOW);
-  pinMode(dripIrrigationRelayPin,OUTPUT);
-  digitalWrite(dripIrrigationRelayPin, LOW);
+  request->send(200, "text/plain", "OK now?");
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  // Initialize SPIFFS
+  if (!SPIFFS.begin())
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
 
   // Store the remaining watering time
   remainingWateringTime = -1;
@@ -91,7 +114,8 @@ void setup() {
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -103,23 +127,22 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-
   // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     Serial.println("'/' called");
 
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
+    request->send(SPIFFS, "/index.html", String(), false, processor); });
+
   // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/style.css", "text/css"); });
 
-
+  server.on("/start2", HTTP_GET, aR1);
 
   // Start the watering
-  server.on("/start", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/start", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     Serial.println("'/start' called");
 
     //web request is as /start?interval=<intervalParameter>
@@ -139,48 +162,83 @@ void setup() {
     // Determine the requested watering interval and start resp. water relay
     switch (resolveParameters(interval))
     {
+
     case OneHour:
+
+      // turn off drip irrigation relay
+      Serial.write(deactivate_relay_two, sizeof(deactivate_relay_two)); 
+      // consecutive serial writes do not work for the relay module
+      // cannot use yield or delay with ESPAsyncWebServer
+      // https://github.com/me-no-dev/ESPAsyncWebServer#important-things-to-remember
+      // workaround: https://github.com/me-no-dev/ESPAsyncWebServer/issues/1190
+      postpone_seconds = millis() + 1 * 1000;
+      while (postpone_seconds > millis()) {
+        // do nothing for 1 second
+      }
+      
       wateringInterval = 1*60*60*1000; // in milliseconds = 1 hour * 60 min * 60 sec * 1000
-      digitalWrite(dripIrrigationRelayPin, LOW); // make sure the drip irrgation relay is off 
-      digitalWrite(sprinklerRelayPin, HIGH); // turn on sprinkler relay
+
+      // turn on sprinkler relay
+      Serial.write(activate_relay_one, sizeof(activate_relay_one));
+      Serial.println("activate_relay_one");
+      sprinklerRelay = 1;
       break;
+
     case EightHours:
+
+      // turn off sprinkler relay
+      Serial.write(deactivate_relay_one, sizeof(deactivate_relay_one)); 
+      postpone_seconds = millis() + 1 * 1000;
+      while (postpone_seconds > millis()) {
+        // do nothing for 1 second
+      }
+
       wateringInterval = 8*60*60*1000; // in milliseconds = 8 hours * 60 min * 60 sec * 1000
-      digitalWrite(sprinklerRelayPin, LOW); // make sure the sprinkler relay is off 
-      digitalWrite(dripIrrigationRelayPin, HIGH); // turn on drip irrigation relay
+
+      // turn on drip irrgation relay
+      Serial.write(activate_relay_two, sizeof(activate_relay_two));
+      Serial.println("activate_relay_two");
+      dripIrrigationRelay = 1;
       break;
+
     case TwelveHours:
+
+      // turn off sprinkler relay
+      Serial.write(deactivate_relay_one, sizeof(deactivate_relay_one)); 
+      postpone_seconds = millis() + 1 * 1000;
+      while (postpone_seconds > millis()) {
+        // do nothing for 1 second
+      }
+
       wateringInterval = 12*60*60*1000; // in milliseconds = 8 hours * 60 min * 60 sec * 1000
-      digitalWrite(sprinklerRelayPin, LOW); // make sure the sprinkler relay is off 
-      digitalWrite(dripIrrigationRelayPin, HIGH); // turn on drip irrigation relay
+
+      // turn on drip irrgation relay
+      Serial.write(activate_relay_two, sizeof(activate_relay_two));
+      Serial.println("activate_relay_two");
+
+      dripIrrigationRelay = 1;
       break;
+
     default:
       break;
     }
-
 
     prevReadingTime = millis();
     remainingWateringTime = prevReadingTime + wateringInterval;
     Serial.print("Water activated, will stop after ");
     Serial.print(remainingWateringTime);
     Serial.println();
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  
-  server.on("/activeInterval", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor); });
+
+  server.on("/activeInterval", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     Serial.println("'/activeInterval' called");
     // return the last set interval
-    request->send(200, "text/plain", interval.c_str());
-  });
+    request->send(200, "text/plain", interval.c_str()); });
 
-  
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     Serial.println("'/off' called");
-
-    // make sure both relays are off
-    digitalWrite(sprinklerRelayPin, LOW);  
-    digitalWrite(dripIrrigationRelayPin, LOW);
 
     // reset interval
     remainingWateringTime = 0;
@@ -188,11 +246,10 @@ void setup() {
     String lastSetInterval = interval; 
     interval = ""; 
     // return the last set interval so UI button gets deactivated
-    request->send(200, "text/plain", lastSetInterval.c_str());
-  });
+    request->send(200, "text/plain", lastSetInterval.c_str()); });
 
-
-  server.on("/remainingWateringTime", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/remainingWateringTime", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     Serial.println("'/remainingWateringTime' called");
     if (remainingWateringTime <= 0)
     {
@@ -200,40 +257,43 @@ void setup() {
     }
     else
     {
-        Serial.println("watering ongoing");
+        //Serial.println("watering ongoing");
     }
     String retval = "{\"remainingSeconds\": " + String(remainingWateringTime/1000) + " ,\"intervalSet\": \"" + interval + "\"}";
     // add to the return value information which watering interval was set
-    request->send(200, "text/plain", retval.c_str());
-  });
+    request->send(200, "text/plain", retval.c_str()); });
 
   // Start server
   server.begin();
-
 }
 
-void loop() {
+void loop()
+{
   // update the remaining time to keep water on
   if (remainingWateringTime > 0)
   {
-      // Serial.println("remaining wtarting time >0"); 
-      long now = millis();
-      remainingWateringTime = remainingWateringTime - (now - prevReadingTime);
-      prevReadingTime = now;
+    // Serial.println("remaining wtarting time >0");
+    long now = millis();
+    remainingWateringTime = remainingWateringTime - (now - prevReadingTime);
+    prevReadingTime = now;
   }
   else if (remainingWateringTime == 0)
-  { 
-      // turn off the water (both relays)
-      digitalWrite(sprinklerRelayPin, LOW);  
-      digitalWrite(dripIrrigationRelayPin, LOW);
-      Serial.println("Water was switched off as watering time completed");
-      remainingWateringTime = -1;
-  }
-  else 
   {
-      // do nothing as relay is not active
+    // turn off the water (both relays)
+    Serial.write(deactivate_relay_one, sizeof(deactivate_relay_one));
+    Serial.println("deactivate_relay_one");
+    sprinklerRelay = 0;
+
+    delay(1000);
+
+    Serial.write(deactivate_relay_two, sizeof(deactivate_relay_two));
+    Serial.println("deactivate_relay_two");
+    dripIrrigationRelay = 0;
+    Serial.println("Water was switched off as watering time completed");
+    remainingWateringTime = -1;
   }
-  
-}  
-
-
+  else
+  {
+    // do nothing as relay is not active
+  }
+}
